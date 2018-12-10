1459a4a092a7e84cdfe0708e7e2bb016d6a68fcf
ireneca <32276508+ireneca@users.noreply.github.com>
2018-12-10 22:02:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
 
     function showPosition(){
 
