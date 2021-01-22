d66e656e25dc3baed1decc0138db14b8ec34f68d
alejandroqm23 <43794737+alejandroqm23@users.noreply.github.com>
2021-01-22 10:02:03 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ document.getElementById("defaultOpen").click();
 
         var mymap = L.map('embedMap').setView([lat, long],13);
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxlamFuZHJvcW0yMyIsImEiOiJja2o5eXZtbzQ1ZHpiMnJuNGx0MW9hc3N5In0.Ld23EBGhSeuOyHT8modfbg', {
 		maxZoom: 16,
 		minZoom: 7,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
